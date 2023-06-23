e1cd57a4173e962028c8f7c2a5eb73b4bdb87db8
Kevind Ospina <kevind171819@gmail.com>
2023-06-23 00:18:34 -0500

    product search infinite scroll fixed

--- src/app/search/product-search/product-search.page.ts
+++ src/app/search/product-search/product-search.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/shared/domain/response/PromotionsData';
 import { RequestUseCases } from 'src/services/domains/usecase/request-use-case';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-product-search',
@@ -11,6 +12,8 @@ export class ProductSearchPage implements OnInit {
 
   pageNumber: number = 1;
   product: Product = {};
+  items: string [] = [];
+  products: any = [];
 
   constructor(
     private requestUseCase: RequestUseCases
@@ -22,11 +25,32 @@ export class ProductSearchPage implements OnInit {
         if (response.success === true) {
           console.log('Promotions: ', response.data);
           this.product = {...response.data.data[0].product}
+          this.products = response.data.data;
         } else {
           console.log('Body del error: ', response);
         }
     })
 
+    this.generateItems();
+
+  }
+
+  // ngOnInit() {
+  //   this.generateItems();
+  // }
+
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev: any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
   }
 
 }
