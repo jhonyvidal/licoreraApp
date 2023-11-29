61391b1620fc34a716a1c5aa65f7d7b364260190
Jhony Vidal <jhonylopez147@hotmail.com>
2023-11-29 11:40:05 -0500

    create account issues

---
+++ src/shared/mask/mask.ts
@@ -0,0 +1,19 @@
+export const dateMask = {
+    mask: [
+        ...Array(2).fill(/\d/),
+        '/',
+        ...Array(2).fill(/\d/),
+        '/',
+        ...Array(4).fill(/\d/),
+    ],
+};
+
+export const phoneMask = {
+    mask: [
+        ...Array(3).fill(/\d/),
+        ' ',
+        ...Array(3).fill(/\d/),
+        ' ',
+        ...Array(4).fill(/\d/),
+    ],
+};
